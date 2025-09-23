f848999770655385264faded45e66f5efdd72e64
ASebastian2 <sebitas.albertos@gmail.com>
2025-09-23 13:33:51 -0500

    Revert "Adds a clicker system"

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ screen = pg.display.set_mode((1280, 720))
 pg.display.set_caption("Hello World!")
 FPS = pg.time.Clock()
 
-#counter variable
-click_count = 0
-
 # Load image
 samosa = pg.image.load(
     "assets/samosa.png"
@@ -23,11 +20,6 @@ while True:
             pg.quit()
             sys.exit()
 
-    #clicker counting
-    if event.type == pg.MOUSEBUTTONDOWN:
-        if samosa_rect.collidepoint(event.pos):
-            click_count += 1
-
     # Fill background (optional, prevents trails)
     screen.fill((30, 30, 30))
 
@@ -37,5 +29,3 @@ while True:
     # Update display
     pg.display.flip()
     FPS.tick(60)
-
-    
