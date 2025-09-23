d1ec118d28785bae9fed3f67dcff9d3a56556310
Nitin Gatla <gatlanit@gmail.com>
2025-09-23 10:24:36 -0500

    Render empty 1280x720 at 60 FPS

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import pygame as pg
+import sys
+
+# Setup
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Hello World!")
+FPS = pg.time.Clock()
+
+# Game Loop
+while True:
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+
+    FPS.tick(60)
+
+    pg.display.flip()
